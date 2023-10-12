0ecf02ddbd7dcedc2e4661ba80cdb59fc8e8d394
Olivier Mignot <olipt.mignot@gmail.com>
2023-10-12 17:23:33 +0200

    created auth forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,5 +2,24 @@
 @section('content')
 <main>
     <h1>Connexion</h1>
+
+    <form action="{{ route('auth.login') }}" method="post">
+        <div class="form-group">
+            <label for="pseudo">Pseudo :</label>
+            <input type="text" name="pseudo" id="pseudo" value="{{ old('pseudo') }}">
+        </div>
+
+        <div class="form-group">
+            <label for="email">E-mail :</label>
+            <input type="email" name="email" id="email" value="{{ old('email') }}">
+        </div>
+
+        <div class="form-group">
+            <label for="password">Mot de passe :</label>
+            <input type="password" name="password" id="password" value="{{ old('password') }}">
+        </div>
+
+        <input type="submit" value="Connexion">
+    </form>
 </main>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,5 +2,34 @@
 @section('content')
 <main>
     <h1>Inscription</h1>
+
+    <form action="{{ route('auth.register') }}" method="post">
+        <div class="form-group">
+            <label for="pseudo">Pseudo :</label>
+            <input type="text" name="pseudo" id="pseudo" value="{{ old('pseudo') }}">
+        </div>
+
+        <div class="form-group">
+            <label for="email">E-mail :</label>
+            <input type="email" name="email" id="email" value="{{ old('email') }}">
+        </div>
+
+        <div class="form-group">
+            <label for="confirmedEmail">Confirmation de l'e-mail :</label>
+            <input type="email" name="confirmedEmail" id="confirmedEmail" value="{{ old('confirmedEmail') }}">
+        </div>
+
+        <div class="form-group">
+            <label for="password">Mot de passe :</label>
+            <input type="password" name="password" id="password" value="{{ old('password') }}">
+        </div>
+
+        <div class="form-group">
+            <label for="confirmedPassword">Confirmation du mot de passe :</label>
+            <input type="confirmedPassword" name="confirmedPassword" id="confirmedPassword" value="{{ old('confirmedPassword') }}">
+        </div>
+
+        <input type="submit" value="S'inscrire">
+    </form>
 </main>
 @endsection
